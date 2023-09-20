12a3b99121c0a6fc20eae37b184264ff44cd0c90
alimarques <ali.mrques@gmail.com>
2023-09-20 15:27:10 -0300

    Salvar arquivos em geojson

--- src/main.py
+++ src/main.py
@@ -3,8 +3,14 @@ import geopandas as gpd
 from geojson import FeatureCollection
 
 extractor = Extractor()
-data = extractor.extract_geojsons(state='São Paulo')
 
-feature_collection = FeatureCollection(data)
+for state in extractor.estados:
+    results = extractor.extract_geojsons(state=state)
+    feature_collection = FeatureCollection(results)
+    gdf_cities = gpd.GeoDataFrame.from_features(feature_collection['features'])
+    gdf_cities.to_file('data/geojson/' + state + '.json', driver='GeoJSON')
+
+results = extractor.extract_geojsons()
+feature_collection = FeatureCollection(results)
 gdf_cities = gpd.GeoDataFrame.from_features(feature_collection['features'])
-gdf_cities.plot()
+gdf_cities.to_json('data/brasil.json')
