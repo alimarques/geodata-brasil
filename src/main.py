25dc51e7aa940212fbb84b0b1757975155bf15f5
alimarques <ali.mrques@gmail.com>
2023-09-20 22:31:19 -0300

    Criar rotinas separadas de municípios e estados

--- src/main.py
+++ src/main.py
@@ -3,14 +3,36 @@ import geopandas as gpd
 from geojson import FeatureCollection
 
 extractor = Extractor()
+municipios_folder = 'data/municipios/'
+state_folder = 'data/estados/'
 
-for state in extractor.estados:
-    results = extractor.extract_geojsons(state=state)
-    feature_collection = FeatureCollection(results)
-    gdf_cities = gpd.GeoDataFrame.from_features(feature_collection['features'])
-    gdf_cities.to_file('data/geojson/' + state + '.json', driver='GeoJSON')
+def city_routine(extract_individuals=False, extract_all=False):
+    if extract_individuals:
+        for state in extractor.estados:
+            results = extractor.extract_geojsons(state=state)
+            feature_collection = FeatureCollection(results)
+            gdf_cities = gpd.GeoDataFrame.from_features(feature_collection['features'])
+            gdf_cities.to_file(municipios_folder + state + '.json', driver='GeoJSON')
 
-results = extractor.extract_geojsons()
-feature_collection = FeatureCollection(results)
-gdf_cities = gpd.GeoDataFrame.from_features(feature_collection['features'])
-gdf_cities.to_file('data/geojson/municipios.json', driver='GeoJSON')
+    if extract_all:
+        results = extractor.extract_geojsons()
+        feature_collection = FeatureCollection(results)
+        gdf_cities = gpd.GeoDataFrame.from_features(feature_collection['features'])
+        gdf_cities.to_file(municipios_folder + 'municipios.json', driver='GeoJSON')
+
+def state_routine(extract_individuals=False, extract_all=False):
+    if extract_individuals:
+        for state in extractor.estados:
+            results = extractor.extract_geojsons(state=state)
+            feature_collection = FeatureCollection(results)
+            gdf_cities = gpd.GeoDataFrame.from_features(feature_collection['features'])
+            gdf_cities.to_file(state_folder + state + '.json', driver='GeoJSON')
+
+    if extract_all:
+        results = extractor.extract_geojsons()
+        feature_collection = FeatureCollection(results)
+        gdf_cities = gpd.GeoDataFrame.from_features(feature_collection['features'])
+        gdf_cities.to_file(state_folder + 'estados.json', driver='GeoJSON')
+
+if __name__ == '__main__':
+    state_routine(extract_all=True)
--- src/params.py
+++ src/params.py
@@ -1,3 +1,4 @@
 URL_MUNICIPIOS = 'https://servicodados.ibge.gov.br/api/v1/localidades/municipios'
-URL_MALHAS = 'https://servicodados.ibge.gov.br/api/v3/malhas/municipios/'
+URL_MALHAS_MUNICIPIOS = 'https://servicodados.ibge.gov.br/api/v3/malhas/municipios/'
+URL_MALHAS_ESTADOS = 'https://servicodados.ibge.gov.br/api/v3/malhas/estados/'
 PROCESSORS = 8
